5c89ef0b3ac63f98d3e862102f8e25dab7449618
Jaco Ahmad <jaco.ahmad@gmail.com>
2018-12-10 02:17:00 +0700

    102 finish

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -13,9 +13,66 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/products_repository.dart';
+import 'model/product.dart';
 
 class HomePage extends StatelessWidget {
   // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if(products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+     locale: Localizations.localeOf(context).toString(),
+    );
+
+    return products.map((product) {
+      return Card(
+        // TODO: Adjust Card Heights (103)
+        child: Column(
+          // TODO: Center Items on the card (103)
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18 / 11,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                fit: BoxFit.fitWidth,
+                // TODO: Adjust the box size (102)
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      product.name,
+                      style: theme.textTheme.title,
+                      maxLines: 1,
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.body2
+                    ),
+                  ],
+                )
+              )
+            )
+          ],
+        ),
+      );
+    }).toList();
+  }
   // TODO: Add a variable for Category (104)
 
   @override
@@ -23,10 +80,37 @@ class HomePage extends StatelessWidget {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
-      // TODO: Add app bar (102)
-      // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+      appBar: AppBar(
+        title: Text('Shrine'),
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () => print('Menu'),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () => print('search button'),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: () => print('filter button'),
+          )
+        ],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2, // specifies how many items across
+        padding: EdgeInsets.all(16.0), // field provides space on all 4 sides of the GridView.
+        childAspectRatio: 8.0 / 9.0, // field identifies the size of the items based on an aspect ratio (width over height).
+        children: _buildGridCards(context),
       ),
     );
   }
